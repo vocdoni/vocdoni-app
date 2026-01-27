0480e03ade1cc8181b7049807e932c7cc730f5fe
Òscar Casajuana <elboletaire@gmail.com>
2026-01-27 11:52:22 +0100

    feat: add local environment helpers

---
+++ src/components/vocdoni-ui/environment.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { environment } from './environment'
+
+describe('environment helpers', () => {
+  it('builds explorer url for dev', () => {
+    expect(environment.explorer('dev')).toBe('https://dev.explorer.vote')
+  })
+})
