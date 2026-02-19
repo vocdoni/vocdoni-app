634ee664f0d2074f54803364485d8a01a23a1b94
Òscar Casajuana <elboletaire@gmail.com>
2026-02-19 17:28:36 +0100

    chore(SuspenseLoader): set proper default styles

--- src/router/SuspenseLoader.tsx
+++ src/router/SuspenseLoader.tsx
@@ -1,4 +1,4 @@
-import { Spinner, Square, SquareProps, Text } from '@chakra-ui/react'
+import { Spinner, Square, type SquareProps, Text } from '@chakra-ui/react'
 import { ReactNode, Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -6,13 +6,17 @@ export const Loading = ({ ...rest }: SquareProps) => {
   const { t } = useTranslation()
 
   return (
-    <Square display='flex' alignItems='center' justifyContent='center' size='full' h='300px' {...rest}>
+    <Square display='flex' alignItems='center' justifyContent='center' size='full' minH='300px' {...rest}>
       <Spinner size='sm' mr={3} />
       <Text>{t('loading')}</Text>
     </Square>
   )
 }
 
-export const SuspenseLoader = ({ children }: { children: ReactNode }) => (
-  <Suspense fallback={<Loading />}>{children}</Suspense>
+type SuspenseLoaderProps = SquareProps & {
+  children: ReactNode
+}
+
+export const SuspenseLoader = ({ children, ...props }: SuspenseLoaderProps) => (
+  <Suspense fallback={<Loading {...props} />}>{children}</Suspense>
 )
