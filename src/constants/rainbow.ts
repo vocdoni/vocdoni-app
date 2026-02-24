fac1de3920d5f8d2ba8ca24db0dfc43b55fe56be
Òscar Casajuana <elboletaire@gmail.com>
2026-02-24 12:59:00 +0100

    chore(rainbow): remove unused connectors
    refs #1541

--- src/constants/rainbow.ts
+++ src/constants/rainbow.ts
@@ -1,7 +1,6 @@
 import { connectorsForWallets } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 import { coinbaseWallet, metaMaskWallet, rainbowWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets'
-import { oAuthWallet, privateKeyWallet, saasOAuthWallet } from '@vocdoni/rainbowkit-wallets'
 import { createConfig, http } from 'wagmi'
 import {
   arbitrum,
@@ -21,7 +20,6 @@ import {
   polygonZkEvm,
   zora,
 } from 'wagmi/chains'
-import i18n from '~i18n'
 
 // Define chains for the application
 const chains = [
@@ -48,67 +46,10 @@ export { chains }
 const appName = 'Vocdoni UI Scaffold'
 const projectId = '641a1f59121ad0b519cca3a699877a08'
 
-export const googleWallet = saasOAuthWallet({
-  id: 'google',
-  name: 'Google',
-  iconUrl: 'https://authjs.dev/img/providers/google.svg',
-  options: {
-    oAuthServiceUrl: import.meta.env.OAUTH_URL,
-    oAuthServiceProvider: 'google',
-    saasBackendUrl: import.meta.env.SAAS_URL,
-  },
-})
-
 const featuredConnectors = [
   {
     groupName: 'Popular',
     wallets: [metaMaskWallet, rainbowWallet, coinbaseWallet, walletConnectWallet],
-  },
-  {
-    groupName: 'Social',
-    wallets: [
-      () =>
-        oAuthWallet({
-          id: 'github',
-          name: 'Github',
-          iconUrl: 'https://authjs.dev/img/providers/github-dark.svg',
-          options: {
-            oAuthServiceUrl: 'https://oauth.vocdoni.io/',
-            oAuthServiceProvider: 'github',
-          },
-        }),
-      () =>
-        oAuthWallet({
-          id: 'google',
-          name: 'Google',
-          iconUrl: 'https://authjs.dev/img/providers/google.svg',
-          options: {
-            oAuthServiceUrl: 'https://oauth.vocdoni.io/',
-            oAuthServiceProvider: 'google',
-          },
-        }),
-      () =>
-        oAuthWallet({
-          id: 'facebook',
-          name: 'Facebook',
-          iconUrl: 'https://authjs.dev/img/providers/facebook.svg',
-          options: {
-            oAuthServiceUrl: 'https://oauth.vocdoni.io/',
-            oAuthServiceProvider: 'facebook',
-          },
-        }),
-      () => googleWallet,
-    ],
-  },
-  {
-    groupName: i18n.t('rainbow.group.recovery'),
-    wallets: [
-      () =>
-        privateKeyWallet({
-          name: i18n.t('rainbow.recovery'),
-          iconUrl: 'https://www.svgrepo.com/show/525392/key-minimalistic-square-3.svg',
-        }),
-    ],
   },
 ]
 
