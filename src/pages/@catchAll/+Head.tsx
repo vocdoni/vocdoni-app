0364416feea420d5425f96669f2801684fd1c850
Òscar Casajuana <elboletaire@gmail.com>
2026-04-21 14:01:16 +0200

    chore(opengraph): add fallback Head file

---
+++ src/pages/@catchAll/+Head.tsx
@@ -0,0 +1,24 @@
+import { AppTitle } from '~constants'
+
+const description =
+  'Vocdoni is an open, secure, and universally verifiable digital voting platform. Create and manage elections, referendums, and governance processes for any organization.'
+
+const image = '/assets/vocdoni_icon.png'
+
+export default function Head() {
+  return (
+    <>
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='Vocdoni' />
+      <meta property='og:title' content={AppTitle} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={image} />
+      <meta name='description' content={description} />
+      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:site' content='@vocdoni' />
+      <meta name='twitter:title' content={AppTitle} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:image' content={image} />
+    </>
+  )
+}
