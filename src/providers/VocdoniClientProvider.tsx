ca9967927ef29e2586dfdf97d2ce3fc6f0455473
Òscar Casajuana <elboletaire@gmail.com>
2026-07-15 16:20:46 +0200

    chore(dead-code): drop legacy client provider, wagmi signer adapter and election adapter
    - VocdoniClientProvider had no consumers left
    - walletClientToSigner (RemoteSigner-era wagmi adapter) is unused
    - toPublishedElection lost its last consumer with the actions migration

--- src/providers/VocdoniClientProvider.tsx
+++
@@ -1,9 +0,0 @@
-import type { ClientProviderProps } from '@vocdoni/react-components'
-import { ClientProvider as BaseClientProvider } from '@vocdoni/react-components'
-import { ToastProvider } from '~components/Toast'
-
-export const ClientProvider = ({ children, ...props }: ClientProviderProps) => (
-  <ToastProvider>
-    <BaseClientProvider {...props}>{children}</BaseClientProvider>
-  </ToastProvider>
-)
