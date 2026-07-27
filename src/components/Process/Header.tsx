41807c0ff9cd79161260844f85acdd6d166b4b46
Òscar Casajuana <elboletaire@gmail.com>
2026-07-27 10:46:55 +0200

    fix(process): drop the unused Gitcoin strategy card, gone with the legacy meta field

--- src/components/Process/Header.tsx
+++ src/components/Process/Header.tsx
@@ -7,11 +7,9 @@ import {
   getElectionDescription,
   useElection,
 } from '@vocdoni/react-components'
-import type { Strategy } from '@vocdoni/sdk'
 import { useTranslation } from 'react-i18next'
 import { useReadMoreMarkdown } from '~components/Layout/use-read-more'
 import { ShareModalButton } from '~components/Share'
-import { StampIcon } from './Census/StampIcon'
 
 const ProcessHeader = () => {
   const { t } = useTranslation()
@@ -96,51 +94,6 @@ const ProcessHeader = () => {
           <ReadMoreMarkdownButton alignSelf='center' />
         </Flex>
       </Flex>
-    </>
-  )
-}
-
-const GitcoinStrategyInfo = () => {
-  const { t } = useTranslation()
-  const { election } = useElection()
-
-  if (!election || !election?.meta?.strategy) {
-    return null
-  }
-
-  const strategy = election.meta.strategy as unknown as Strategy
-  const score = strategy.tokens['GPS'].minBalance
-  const firstParenthesesMatch = strategy.predicate.match(/\(([^)]+)\)/)
-  let unionTypeString: string | null = null
-  if (firstParenthesesMatch) {
-    // split by space and get the second element which should be the union type
-    const [, unionType] = firstParenthesesMatch[1].split(' ')
-    if (unionType === 'AND') {
-      unionTypeString = t('process.gitcoin.all_of_them')
-    } else if (unionType === 'OR') {
-      unionTypeString = t('process.gitcoin.one_of_them')
-    }
-  }
-  const tokens = Object.entries(strategy.tokens).filter(([key, token]) => key !== 'GPS')
-
-  return (
-    <>
-      <Text>{t('process.gitcoin.gps_score', { score: score })}</Text>
-      {unionTypeString && (
-        <Flex direction={'column'} gap={2} mt={4}>
-          <Flex direction={{ base: 'column', lg: 'row' }} gap={{ base: 0, lg: 1 }}>
-            <Text fontWeight='bold'>{t('process.gitcoin.needed_stamps')}</Text>
-            <Text>{unionTypeString}</Text>
-          </Flex>
-          <Flex direction={'row'} gap={1} flexWrap={'wrap'}>
-            {Object.values(tokens).map(([, token], n) => {
-              return (
-                <StampIcon key={n} size={6} iconURI={token.iconURI} alt={token.externalID} tooltip={token.externalID} />
-              )
-            })}
-          </Flex>
-        </Flex>
-      )}
     </>
   )
 }
