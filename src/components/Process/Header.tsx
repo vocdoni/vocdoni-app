6805d6ce8e57d0c544e32b608529e49f1d112b45
Òscar Casajuana <elboletaire@underave.net>
2026-09-16 10:23:11 +0200

    chore(process): remove the social share button and modal (#1791)

--- src/components/Process/Header.tsx
+++ src/components/Process/Header.tsx
@@ -9,7 +9,6 @@ import {
 } from '@vocdoni/react-components'
 import { useTranslation } from 'react-i18next'
 import { useReadMoreMarkdown } from '~components/Layout/use-read-more'
-import { ShareModalButton } from '~components/Share'
 
 const ProcessHeader = () => {
   const { t } = useTranslation()
@@ -43,46 +42,16 @@ const ProcessHeader = () => {
       )}
       <Flex flexDir='column'>
         <ElectionTitle fontSize='4xl' textAlign='left' mb={5} />
-        <Flex
-          flexDirection={{ base: 'column', xl: 'row' }}
-          flexWrap={{ base: 'nowrap', xl: 'wrap' }}
-          mb={4}
-          justifyContent='space-between'
-          columnGap={4}
-          rowGap={3}
-        >
-          <Flex
-            gap={2}
-            flexDirection={{ base: 'column', xl: 'row' }}
-            alignItems={{ base: 'start', xl: 'center' }}
-            flex='1'
-          >
-            <Flex gap={3} justifyContent='space-between' w={{ base: 'full', xl: 'fit-content' }}>
-              <ElectionStatusBadge whiteSpace='nowrap' />
-              <Box display={{ base: 'flex', xl: 'none' }}>
-                <ShareModalButton
-                  caption={t('share.election_share_text')}
-                  text={t('share.election_share_btn_text')}
-                  size='xs'
-                />
-              </Box>
-            </Flex>
-            <ElectionSchedule
-              textAlign='left'
-              color='process.info_title.light'
-              _dark={{ color: 'process.info_title.dark' }}
-              display='block'
-              fontSize='sm'
-              lineHeight='short'
-            />
-          </Flex>
-          <Box display={{ base: 'none', xl: 'flex' }} flexShrink={0}>
-            <ShareModalButton
-              caption={t('share.election_share_text')}
-              text={t('share.election_share_btn_text')}
-              size='xs'
-            />
-          </Box>
+        <Flex gap={2} flexDirection={{ base: 'column', xl: 'row' }} alignItems={{ base: 'start', xl: 'center' }} mb={4}>
+          <ElectionStatusBadge whiteSpace='nowrap' />
+          <ElectionSchedule
+            textAlign='left'
+            color='process.info_title.light'
+            _dark={{ color: 'process.info_title.dark' }}
+            display='block'
+            fontSize='sm'
+            lineHeight='short'
+          />
         </Flex>
         <Flex flexDirection='column'>
           {!getElectionDescription(election)?.length && <Text color='fg.muted'>{t('process.no_description')}</Text>}
